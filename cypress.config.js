9e9e3a9256d6d556ab2d2389f47e8e3cb3d753de
Kaio Gabriel <kaiogabrields@hotmail.com>
2024-06-24 17:31:48 -0300

    set timeout padrao

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 5000, // alterando o timeout padrao do cypress
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
